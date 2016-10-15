c9f3771a3c7a493ee4da6fab21cbe36b09ad0b02
Iryna Sergiychuk <ira_24@ukr.net>
2016-10-15 12:40:21 +0000

    add new file

--- index.js
+++ index.js
@@ -41,3 +41,13 @@ $(document).ready(function(){
 $(window).resize(function() {
   $('.item').css('height', $(window).height());
 });
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
